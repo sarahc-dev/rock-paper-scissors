02195b209f5d03ed053771a50455a0b94a3bb2d9
sazzledazzle <sarahclements@hotmail.co.uk>
2021-01-05 20:27:29 +0000

    game basic functions working

--- game.js
+++ game.js
@@ -1,23 +1,96 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
 const options = ["rock", "paper", "scissors"];
+const widthQuery = window.matchMedia("(min-width: 600px)");
 
 let gameChoice = "";
 let userChoice = "";
 
-document.querySelectorAll(".game-choice").forEach((button) => {
+$$(".circle-btn-outer").forEach((button) => {
     button.addEventListener("click", () => {
-        document.querySelector(".user-choice").appendChild(button);
+        userChoice = button.getAttribute("value");
+        newGame();
     })
 })
 
 function newGame() {
-   
+
+   $(".game").style.background = "none";
+   $(".bottom-btns").classList.add("hidden");
+   $(".scissors-outer").classList.add("hidden");
+   $(".opaque-circle").classList.remove("hidden");
+
+    let userBtn = userChoice + "-outer";
+    let newImg = "./images/icon-" + userChoice + ".svg";
+
+    $(".circle-btn-outer").classList.replace("paper-outer", userBtn);
+    $(".circle-btn").classList.replace("paper", userChoice);
+    $(".btn-img").setAttribute("src", newImg);
+    $(".btn-img").setAttribute("alt", userChoice);
+
+   const youPicked = document.createElement("h3");
+   youPicked.textContent = "You picked";
+   const housePicked = document.createElement("h3");
+   housePicked.textContent = "The house picked";
+   housePicked.className = "house";
+
+   if (widthQuery.matches === false) {
+       $(".circle-btn-outer").appendChild(youPicked);
+       $(".circle-btn-outer").disabled = true;
+       $(".opaque-circle").appendChild(housePicked);
+   } else {
+       let parent = $(".circle-btn-outer").parentNode;
+       parent.insertBefore(youPicked, $(".circle-btn-outer"));
+    $(".circle-btn-outer").disabled = true;
+    $("h3").append(housePicked);
+   }
+
+   setTimeout(() => {
+    housePicks();
+}, 1000);
+}
+
+function housePicks() {
 
     let randomNumber = Math.floor(Math.random() * 3);
     gameChoice = options[randomNumber];
-    let computerImg = "./images/icon-" + gameChoice + ".svg";
 
-    document.querySelector(".computer-choice").setAttribute("src", computerImg);
-    document.querySelector(".computer-choice").setAttribute("alt", gameChoice);
+    let houseBtn = gameChoice + "-outer";
+    let houseImg = "./images/icon-" + gameChoice + ".svg";
+
+    $(".opaque-circle").classList.add("hidden");
+    $("[value='scissors']").classList.remove("hidden");
+
+    if (gameChoice !== "scissors") {
+        $("[value='scissors']").classList.replace("scissors-outer", houseBtn);
+        $("[value='scissors'] .circle-btn").classList.replace("scissors", gameChoice);
+        $("[value='scissors'] .circle-btn .btn-img").setAttribute("src", houseImg);
+        $("[value='scissors'] .circle-btn .btn-img").setAttribute("alt", gameChoice);
+    }
+
+    setTimeout(() => {
+        whoWins();
+    }, 1000);
     
 }
+
+function whoWins() {
+
+    $(".result").classList.remove("hidden");
+   
+    if (userChoice == gameChoice) {
+        $("h1").innerHTML = "Draw";
+    } else if ((userChoice == "paper" && gameChoice == "rock") || (userChoice == "scissors" && gameChoice == "paper") || (userChoice == "rock" && gameChoice == "scissors")) {
+        $("h1").innerHTML = "You win";
+    } 
+
+    $(".play-again").addEventListener("click", () => {
+        location.reload();
+    })
+}
+    
+
+    
+
 
